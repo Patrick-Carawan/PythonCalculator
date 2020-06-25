7dcc7740c83aaa9308710c82dfed8aa92ace5cab
Patrick Dane Carawan <pat1@live.unc.edu>
2020-06-25 15:33:22 -0400

    Switched to classes

--- Calculator/__init__.py
+++ Calculator/__init__.py
@@ -1,13 +1,16 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QLabel
 
+class Calculator():
+    def __init__(self):
+        self.window()
 
-def window():
-    app = QApplication([])
-    label = QLabel('Hello World!')
-    label.show()
-    app.exec_()
+    def window(self):
+        app = QApplication([])
+        label = QLabel('Hello World!')
+        label.show()
+        app.exec_()
 
 
 if __name__ == '__main__':
-    window()
+    Calculator()
