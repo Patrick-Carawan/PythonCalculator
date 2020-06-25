e3af0948cba251b0ce4c083f2080ddb9fa369407
Patrick Dane Carawan <pat1@live.unc.edu>
2020-06-25 15:28:23 -0400

    Initial commit

---
+++ Calculator/__init__.py
@@ -0,0 +1,13 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel
+
+
+def window():
+    app = QApplication([])
+    label = QLabel('Hello World!')
+    label.show()
+    app.exec_()
+
+
+if __name__ == '__main__':
+    window()
